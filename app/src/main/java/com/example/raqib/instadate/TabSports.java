625ce0aa30e948eb43553e5bd4b55f0c28273cbc
Raqib Ahmad <raqib850@gmail.com>
2016-06-26 21:56:45 +0530

    created the specific categories in particulat tabs

---
+++ app/src/main/java/com/example/raqib/instadate/TabSports.java
@@ -0,0 +1,46 @@
+package com.example.raqib.instadate;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class TabSports extends Fragment {
+
+    RecyclerView myRecyclerView;
+    public List<NewsItems> NYT;
+    public List<NewsItems> ESPN;
+    public List<NewsItems> TFE;
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.general_recycler_view,container,false);
+        myRecyclerView = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+        myRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+        myRecyclerView.hasFixedSize();
+
+        ESPN = SitesXmlPullParserEspnCricinfo.getStackSitesFromFile(getActivity().getBaseContext());
+        List<NewsItems> newsItemsList = new ArrayList<NewsItems>(){
+            {
+                addAll(ESPN);
+            }
+        };
+
+            myRecyclerView.setAdapter(new MyNewsRecyclerViewAdapter(newsItemsList));
+
+
+        return view;
+    }
+
+
+}
